22280aca1b817cf520cdd899524c566597f9423d
Gutuleac Vlad <vladgutuleac@gmail.com>
2020-05-09 17:02:19 +0300

    dict

--- tmp.py
+++ tmp.py
@@ -1,1 +1,66 @@
 print('hi')
+
+#Numbers
+
+x = 50
+y = 100
+
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x//y)
+print(x%y)
+print(-x)
+print(abs(x))
+print (divmod(x,y))
+print(x**y)
+
+#Lines
+
+S1 = 'vlad'
+S2 = 'gutuleac'
+
+print(S1+S2)
+print('vlad'*3)
+len('vlad')
+
+S = 'spam'
+S[0]
+S[2]
+S[-2]
+
+s = 'vladgutuleac'
+s[3:5]
+s[2:-2]
+s[:6]
+
+#List
+
+list = ('a','b','c')
+
+list.count('b')
+
+list.insert(b,x)
+
+list.remove('a')
+
+
+#Index
+
+a = [1, 3, 8, 7]
+a[1]
+a[2]
+a[4]
+a[-1]
+a[-4]
+a[-5]
+a[1:]
+a[:]
+a[:3]
+a[::2]
+a[::-1]
+a[:-2]
+a[-2::-1]
+
+
